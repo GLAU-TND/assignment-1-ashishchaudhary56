289b8e18cc0f12cd0a2c69c1ca5aa556a3c22317
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2020-02-24 22:06:33 +0530

    main class created

--- src/definition/Definition.java
+++ src/definition/Definition.java
@@ -1,7 +1,14 @@
 package definition;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Definition {
-
+    public void addNumber(){
+        ArrayList<String> numbers=new ArrayList<>();
+        String Name="";
+        String LastName="";
+        String email="";
+        String phoneNumber="";
+    }
 }
