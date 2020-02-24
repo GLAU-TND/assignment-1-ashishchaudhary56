606e687331222f62d5c8dff661d39073f1c67b43
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2020-02-24 22:36:30 +0530

    implement of methods

--- src/definition/Definition.java
+++ src/definition/Definition.java
@@ -3,12 +3,17 @@ package definition;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Definition {
+public class Definition implements Contact {
+
+}
     public void addNumber(){
         ArrayList<String> numbers=new ArrayList<>();
         String Name="";
         String LastName="";
         String email="";
         String phoneNumber="";
+    public void showNumber(){
+
     }
+
 }
