8445fb32af9709d167ea66b2ef993872849353bd
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2020-02-24 22:47:15 +0530

    body for methods

--- src/definition/Definition.java
+++ src/definition/Definition.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Definition implements Contact {
-
-}
+    @Override
     public void addNumber(){
         ArrayList<String> numbers=new ArrayList<>();
         String Name="";
         String LastName="";
         String email="";
         String phoneNumber="";
+
+
+
     public void showNumber(){
 
     }
+    public void searchNumber(){
+
+        }
+        public void removeNumber(){
 
 }
